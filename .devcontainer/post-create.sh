2d422aa7ca57e45f6261aa7e7585a1e73623a7c1
Jay Withers <jay.withers@appvia.io>
2026-05-29 10:42:03 +0000

    fix: git commit within dev container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,5 +5,7 @@ set -e
 . .devcontainer/.git-env
 git config --global user.name  "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
+# Required when the repo is bind-mounted and owned by a different UID than the container user
+git config --global --add safe.directory /workspaces/dev-containers-demo
 
 pre-commit install
